de87faac09afd4818fe64ac4c3caaa773ecc741a
Alejandro <jairoalejandro2003@gmail.com>
2025-03-01 13:16:51 -0500

    google maps api key setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,7 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
-import "react-native-reanimated";
+import "../global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
--- app/maps/index.tsx
+++ app/maps/index.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
 const MapsScreen = () => {
   return (
-    <View>
-      <Text>MapsScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1">
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "red",
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+          }}
+          title="Initial Position"
+          description="This is the initial position"
+        />
+      </MapView>
+    </SafeAreaView>
+  );
+};
 
-export default MapsScreen
+export default MapsScreen;
